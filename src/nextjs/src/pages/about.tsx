72ddca9d623693708be78e1aa5c87ed3c509ddc5
Tim Schopf <tim.schopf@t-online.de>
2024-08-30 16:20:43 +0200

    Update citation information

--- src/nextjs/src/pages/about.tsx
+++ src/nextjs/src/pages/about.tsx
@@ -189,7 +189,8 @@ const About = () => {
         <section>
           <h2 className="text-2xl font-bold mb-4">Citation Information</h2>
           <p>
-            You can find more details in our ACL 2024 Paper <a href="https://arxiv.org/abs/2406.15294" target="_blank"
+            You can find more details in our ACL 2024 Paper <a href="https://aclanthology.org/2024.acl-demos.13"
+                                                               target="_blank"
                                                                rel="noopener noreferrer"
                                                                className="text-blue-500 hover:underline">NLP-KG: A
             System for Exploratory Search of Scientific Literature in Natural Language Processing</a>. When citing our
@@ -197,15 +198,22 @@ const About = () => {
           </p>
           <pre className="bg-gray-100 p-4 overflow-x-auto">
             <code className="text-sm">
-            {`@misc{schopf2024nlpkgexploratorysearchscientific,
-                    title={NLP-KG: A System for Exploratory Search of Scientific Literature in Natural Language Processing}, 
-                    author={Tim Schopf and Florian Matthes},
-                    year={2024},
-                    eprint={2406.15294},
-                    archivePrefix={arXiv},
-                    primaryClass={cs.CL},
-                    url={https://arxiv.org/abs/2406.15294}, 
-              }`}
+            {`@inproceedings{schopf-matthes-2024-nlp,
+                title = "{NLP}-{KG}: A System for Exploratory Search of Scientific Literature in Natural Language Processing",
+                author = "Schopf, Tim  and
+                  Matthes, Florian",
+                editor = "Cao, Yixin  and
+                  Feng, Yang  and
+                  Xiong, Deyi",
+                booktitle = "Proceedings of the 62nd Annual Meeting of the Association for Computational Linguistics (Volume 3: System Demonstrations)",
+                month = aug,
+                year = "2024",
+                address = "Bangkok, Thailand",
+                publisher = "Association for Computational Linguistics",
+                url = "https://aclanthology.org/2024.acl-demos.13",
+                pages = "127--135",
+                abstract = "Scientific literature searches are often exploratory, whereby users are not yet familiar with a particular field or concept but are interested in learning more about it. However, existing systems for scientific literature search are typically tailored to keyword-based lookup searches, limiting the possibilities for exploration. We propose NLP-KG, a feature-rich system designed to support the exploration of research literature in unfamiliar natural language processing (NLP) fields. In addition to a semantic search, NLP-KG allows users to easily find survey papers that provide a quick introduction to a field of interest. Further, a Fields of Study hierarchy graph enables users to familiarize themselves with a field and its related areas. Finally, a chat interface allows users to ask questions about unfamiliar concepts or specific articles in NLP and obtain answers grounded in knowledge retrieved from scientific publications. Our system provides users with comprehensive exploration possibilities, supporting them in investigating the relationships between different fields, understanding unfamiliar concepts in NLP, and finding relevant research literature. Demo, video, and code are available at: https://github.com/NLP-Knowledge-Graph/NLP-KG-WebApp.",
+            }`}
             </code>
           </pre>
         </section>
